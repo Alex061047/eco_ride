d978987b50a38999118a6b4868fc6059b2244c43
Alex N <alexnoble47@gmail.com>
2025-04-07 01:18:42 +0200

    Gestion navbar

---
+++ Controleur/navbar/navbar.js
@@ -0,0 +1,27 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const monEspaceItem = document.getElementById('mon-espace-item');
+    const logoutItem = document.getElementById('logout-item');
+    const loginItem = document.getElementById('login-item');
+    const logoutLink = document.getElementById('logout-link');
+
+    const isLoggedIn = sessionStorage.getItem("isLoggedIn") === "true";
+
+    if (isLoggedIn) {
+        monEspaceItem.style.display = "block";
+        logoutItem.style.display = "block";
+        loginItem.style.display = "none";
+    } else {
+        monEspaceItem.style.display = "none";
+        logoutItem.style.display = "none";
+        loginItem.style.display = "block";
+    }
+
+    // Gestion du clic sur "Déconnexion"
+    if (logoutLink) {
+        logoutLink.addEventListener("click", (e) => {
+            e.preventDefault();
+            sessionStorage.removeItem("isLoggedIn");
+            window.location.href = "/"; // Redirection vers accueil
+        });
+    }
+});
